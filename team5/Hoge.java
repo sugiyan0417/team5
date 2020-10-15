d204a74eb9f114e8dfd7e0dc27cadb94f2ec16da
大根田 みらの <16022@kauth.local>
2020-10-15 10:09:04 +0900

    Bがキャラ追加

--- team5/Hoge.java
+++ team5/Hoge.java
@@ -14,6 +14,9 @@ public class Hoge extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+}
     }    
 }
