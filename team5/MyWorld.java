8b7bbe40355ee6660411c498882bb02f36356b9b
大根田 みらの <16022@kauth.local>
2020-12-10 01:03:44 +0000

    B return2

--- team5/MyWorld.java
+++ team5/MyWorld.java
@@ -1,6 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.util.List;
-import java.util.ArrayList;
+
 /**
  * Write a description of class MyWorld here.
  * 
@@ -15,19 +14,13 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    //List<Actor> actors = new ArrayList<Actor>();
-    //List<Player> players = new ArrayList<Player>();
-    Player player = new Player();
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1000, 750, 1);
+
+        super(1000, 900, 1); 
         addObject( new key(), 100, 250 );
         addObject( new door(), 700, 169 );
-<<<<<<< HEAD
-        addObject( new key(), 200, 680 );
-        ///addObject(new Player(), 100, 680);
-=======
         //addObject( new door(), 350, 680 );//テスト用
         //addObject( new key(), 300, 680 );//テスト用
         addObject( new slime(), 300, 680 );//テスト用
@@ -38,7 +31,6 @@ public class MyWorld extends World
         addObject(new life2(), 800, 800);
         addObject(new life3(), 900, 800);
         
->>>>>>> (大根田：画像＆コード追加)
         
         for(int x=50;x<1000;x+=150){
         addObject( new block_seaground(), x, 830 );
@@ -52,20 +44,6 @@ public class MyWorld extends World
     for(int x=700;x<1500;x+=150){
         addObject( new block_seaground(), x, 300 );
     }
-<<<<<<< HEAD
-         
-        addObject(player, 500, 650);
-        //players = getObjects(Player.class);
-        //addObject(new Player(), 500, 650);
-    }
-    public void act(){
-       //showText(String.valueOf(players.get(0).getX()),100,100);
-       showText(String.valueOf(player.getX()),100,100);
-       showText(String.valueOf(player.getY()),200,200);
-       int x = player.getX();
-       int y = player.getY();
-       player.setLocation(x,y+4);
-=======
     
       
 
@@ -73,7 +51,6 @@ public class MyWorld extends World
     public void act(){
         setActOrder(block_seaground.class,life1.class, life2.class, life3.class);
       
->>>>>>> (大根田：画像＆コード追加)
     }
 
 }
